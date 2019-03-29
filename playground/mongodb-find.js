b053a6773f250cae776f808827f2f5ca91a05bda
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-03-29 09:36:38 +0600

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to the mongodb database', err);
+    }
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c9d7ef3b0d6bfbd852bb183')
+    // }).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos Count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'John'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+});
