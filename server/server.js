05242dd2cf8dc307ba3a172ba437fe92316d8094
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-03-29 15:28:50 +0600

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        res.send({todos, code: 200});
+    }, err => {
+        res.status(400).send(err);
+    });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
